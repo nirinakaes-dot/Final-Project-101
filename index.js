f67a32c22e6a7ffda5140f229d260804ae21ee0e
Nina Adora <nirinakaes@gmail.com>
2026-04-17 00:17:06 +0300

    Update search input ID and enhance table display functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Api
-const tablesApi = "https://api.football-data.org/v4/competitions/PL/standings";
+const proxy = "https://corsproxy.io/?"
+// const tablesApi = "https://api.football-data.org/v4/competitions/";
 // const fixturesApi= ""
 // const statsApi=""
 
@@ -7,26 +8,31 @@ const apiKey= "b6f8f72738dd4e7789c10e7580e01337"
 /////////////////////////////////////////adding event listener////////////////////////////////
 
 const tablesButton= document.querySelector('#tableBtn')
-const tableDisplay= document.querySelector('#table')
 
+
+tablesButton.addEventListener('click', displayTables)
 
 ////////////////////////////Display tableStandings function///////////////////////////////////////
 
 async function displayTables(){
 
-    const tablesDisplay= document.querySelector('#tableStandings')
+    const tablesDisplay= document.querySelector('#table')
+    const searchInput = document.querySelector('#tables').value.trim().toUpperCase()
 
     if(!tablesDisplay) return;
 
     try{
 
-    const response= await fetch(tablesApi,
+    const response= await fetch(`${proxy}https://api.football-data.org/v4/competitions/${searchInput}/standings`,
         {
             headers: {'X-Auth-Token': apiKey}
         }
     )
+    if(!response.ok){throw new Error("Failed to get standings")}
+
     const data= await response.json()
     console.log(data)
+
     const standings= data.standings[0].table;
     console.log(standings)
 
@@ -64,7 +70,6 @@ async function displayTables(){
 
 }
 
-displayTables()
 
 // /////////////////////////////////////////display fixtures///////////////////////////////////////////
 
